6afd2d18acfad3d4e5fc0c34191d3abdbe8fd6aa
YatsuGashir <yatsugashirdev@gmail.com>
2025-04-06 13:20:56 +0300

    В процессе добовления upgradeManager

--- Assets/scripts/UpgradeManager.cs
+++ Assets/scripts/UpgradeManager.cs
@@ -1,19 +1,49 @@
-using System;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour
 {
-    private void Update()
+    public static UpgradeManager instance;
+
+    [Header("UI элементы")]
+    [SerializeField] private TextMeshProUGUI oreCountText;
+    [SerializeField] private TextMeshProUGUI upgradeCostText;
+    [SerializeField] private Button upgradeButton;
+
+    private int oreCount = 0;
+    private int upgradeCost = 2; // Примерная стоимость улучшения
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            DrillController.Instance.SelectDrill(1);
-        }
+        instance = this;
     }
 
-    public void DrillUpgrade()
+    private void Update()
     {
-        DrillController.Instance.SelectDrill(1);
+        // Обновляем UI с количеством руды
+        oreCountText.text = "Руда: " + oreCount;
+
+        // Обновляем стоимость улучшения
+        upgradeCostText.text = "Стоимость улучшения: " + upgradeCost;
+
+        // Включаем или выключаем кнопку в зависимости от количества руды
+        upgradeButton.interactable = oreCount >= upgradeCost;
+    }
+
+    public void AddOre(int amount)
+    {
+        oreCount += amount;
+    }
+
+    public void UpgradeDrill()
+    {
+        if (oreCount >= upgradeCost)
+        {
+            oreCount -= upgradeCost; // Снимаем стоимость улучшения
+            DrillController.Instance.SelectDrill(1); // Применяем улучшение
+
+            // Можете добавить сюда дополнительные улучшения
+        }
     }
 }
